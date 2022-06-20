e3ab16d8cf05000245bc6649443d09243c636d07
Nils Rommelfanger <n.rommelfanger@clevershuttle.de>
2022-06-21 00:39:13 +0200

    comments

--- src/test/kotlin/com/example/codingchallenge/MergerTest.kt
+++ src/test/kotlin/com/example/codingchallenge/MergerTest.kt
@@ -24,9 +24,14 @@ class MergerTest {
                 Arguments.of( listOf(Pair(1,2)), listOf(Pair(1,2))),
                 Arguments.of( listOf(Pair(1,2),Pair(4,5)), listOf(Pair(1,2),Pair(4,5))),
                 Arguments.of( listOf(Pair(1,2),Pair(3,4)), listOf(Pair(1,4))),
+                Arguments.of( listOf(Pair(1,2),Pair(2,4)), listOf(Pair(1,4))),
+                // merge back and forth. Third interval merges the first two together
                 Arguments.of( listOf(Pair(1,2),Pair(5,6),Pair(3,4)), listOf(Pair(1,6))),
+                Arguments.of( listOf(Pair(1,2),Pair(3,4),Pair(2,3)), listOf(Pair(1,4))),
+                // ordering
                 Arguments.of( listOf(Pair(1,2),Pair(8,9),Pair(4,5)), listOf(Pair(1,2), Pair(4,5), Pair(8,9))),
                 Arguments.of( listOf(Pair(8,9),Pair(4,5),Pair(1,2)), listOf(Pair(1,2), Pair(4,5), Pair(8,9))),
+                // a special case where successive intervals need to replace the first node
                 Arguments.of( listOf(Pair(8,9),Pair(3,4),Pair(1,2)), listOf(Pair(1,4), Pair(8,9))),
 
         )
